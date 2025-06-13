bb788e3bf45f58905195d93d4766b369da3f321b
abdulmannan002 <abdulmannan0347@gmail.com>
2025-06-13 22:31:27 +0500

    Update merchant ID mappings in uidMap for clarity

--- status_inquiry.js
+++ status_inquiry.js
@@ -77,9 +77,9 @@ const uidMap = {
   140: "cc961e51-8c0e-44d4-9c25-56e39e992b88",
   // JazzCash Merchant IDs
   7: "6d612b47-6405-4237-9b0c-7d639eb960ee", // SASTA TECH SOLUTIONS
-  11: "a0eb8ba1-8962-4766-8acb-945fce7dc0c3", // DEVINERA TECHNOLOGIES
-  32: "3c0ba58b-5a69-4376-b40d-4d497d561ba2", // THINK TECH CONSULTANCY
-  27: "cc961e51-8c0e-44d4-9c25-56e39e992b88" // NEXTERA SPHERE
+  11: "a0eb8ba1-8962-4766-8acb-945fce7dc0c3", //  TECHNOLOGIES
+  32: "3c0ba58b-5a69-4376-b40d-4d497d561ba2", // learning CONSULTANCY
+  27: "cc961e51-8c0e-44d4-9c25-56e39e992b88" // evolivca SPHERE
 };
 
 // Function to handle the transaction

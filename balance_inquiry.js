2f8d90217c58b1c3b568b968b91debdf2f91f481
Abdul Mannan <84622390+abdulmannan002@users.noreply.github.com>
2025-08-27 13:58:31 +0500

    Update balance_inquiry.js

--- balance_inquiry.js
+++ balance_inquiry.js
@@ -47,7 +47,7 @@ async function processQueue() {
 }
 
 const DASHBOARD_API_URLS = [
-  'https://server.sahulatpay.com/dashboard/merchant-details',
+  'https://api.sahulatpay.com/dashboard/merchant-details',
   'https://api5.assanpay.com/api/dashboard/merchant-details',
 ];
 
